077ae414aee12b0f03f90dca571555943a68a763
Yordanov <iliyanyyordanov@gmail.com>
2020-08-12 14:13:49 +0100

    Issue #10

--- admin_panel/users_update.php
+++ admin_panel/users_update.php
@@ -1,49 +1,15 @@
 <?php
+    session_start();
+    include_once('../functions.php');
+    connectToDatabase($_SESSION['Database']);
 
-    function updateRecordName(mysqli $db, $newName, $id){
-        if(isset($newName)){
-            $sql = "UPDATE `users` SET Username= '".$newName."' WHERE ID = '".$id."'";
-            $result = $db->query($sql);
-            if($result){
-                header("Location: admin_panel.php?page=users&success=true&nm=Username");
-            }
-            if(!$result){
-                header("Location: admin_panel.php?page=users&success=false&nm=Username");
-            }
-        }
-    }
-    function updateRecordPass(mysqli $db, $newPass, $id){
-        if(isset($newPass)){
-            $sql = "UPDATE `users` SET Password= '".md5($newPass)."' WHERE ID = '".$id."'";
-            $result = $db->query($sql);
-            if($result){
-                header("Location: admin_panel.php?page=users&success=true&nm=Password");
-            }
-            if(!$result){
-                header("Location: admin_panel.php?page=users&success=false&nm=Password");
-            }
-        }
-    } 
-    function updateRecordEmail(mysqli $db, $newEmail, $id){
-        if(isset($newEmail)){
-            $sql = "UPDATE `users` SET Email= '".$newEmail."' WHERE ID = '".$id."'";
-            $result = $db->query($sql);
-            if($result){
-                header("Location: admin_panel.php?page=users&success=true&nm=Email");
-            }
-            if(!$result){
-                header("Location: admin_panel.php?page=users&success=false&nm=Email");
-            }
-        }
-    } 
     $id = $_POST['id'];
     $newName = $_POST['newName'];
     $newPass = $_POST['newPass'];
     $newEmail = $_POST['newEmail'];
     
+    //Functions which are updating the database 
     if(!empty($newName)){updateRecordName($conn,$newName, $id);}
     if(!empty($newPass)){updateRecordPass($conn,$newPass, $id);}
     if(!empty($newEmail)){updateRecordEmail($conn, $newEmail, $id);}
-    
-
 ?>
--- functions.php
+++ functions.php
@@ -19,4 +19,48 @@ function connectToDatabase($database){
     
     
 }
+//Function for updating username into the database
+function updateRecordName(mysqli $db, $newName, $id){
+    if(isset($newName)){
+        $sql = "UPDATE `users` SET Username= '".$newName."' WHERE ID = '".$id."'";
+        $result = $db->query($sql);
+        if($result){
+            header("Location: admin_panel.php?page=team&success=true&nm=Username");
+        }
+        if(!$result){
+            header("Location: admin_panel.php?page=team&success=false&nm=Username");
+            
+            
+        }
+    }
+}
+
+//Function to update the password into the database
+function updateRecordPass(mysqli $db, $newPass, $id){
+    if(isset($newPass)){
+        $sql = "UPDATE `users` SET Password= '".md5($newPass)."' WHERE ID = '".$id."'";
+        $result = $db->query($sql);
+        if($result){
+            header("Location: admin_panel.php?page=team&success=true&nm=Password");
+        }
+        if(!$result){
+            header("Location: admin_panel.php?page=team&success=false&nm=Password");
+            echo "Error updating record: " . $conn->error;
+        }
+    }
+} 
+//Function to update the email into the database
+function updateRecordEmail(mysqli $db, $newEmail, $id){
+    if(isset($newEmail)){
+        $sql = "UPDATE `users` SET Email= '".$newEmail."' WHERE ID = '".$id."'";
+        $result = $db->query($sql);
+        if($result){
+            header("Location: admin_panel.php?page=team&success=true&nm=Email");
+        }
+        if(!$result){
+            header("Location: admin_panel.php?page=team&success=false&nm=Email");
+            echo "Error updating record: " . $conn->error;
+        }
+    }
+} 
 ?>
