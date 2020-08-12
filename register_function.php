63793fd4e4f675b77d4f049e2b917fb21f0c2f54
Yordanov <iliyanyyordanov@gmail.com>
2020-08-12 11:01:06 +0100

    Issue #7 Changing colun names new databases table USERS

--- register_function.php
+++ register_function.php
@@ -40,11 +40,11 @@
             mysqli_close($conn);
             connectToDatabase($databaseName);
             $sql = "CREATE TABLE users (
-                id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-                firstname VARCHAR(30) NOT NULL,
-                lastname VARCHAR(30) NOT NULL,
-                email VARCHAR(50),
-                reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+                Project VARCHAR(30) NOT NULL,
+                Username VARCHAR(30) NOT NULL,
+                Password VARCHAR(300) NOT NULL,
+                Email VARCHAR(50),
+                RegDate TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
                 )";
             if(mysqli_query($conn, $sql)) {
 
